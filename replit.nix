c0666d433cb6abef573d0e8d008af55f323b67b0
Taehun Lee <116613476+thiskorea81@users.noreply.github.com>
2023-01-18 15:43:09 +0000

    add c

--- replit.nix
+++ replit.nix
@@ -1,18 +1,8 @@
 { pkgs }: {
-  deps = [
-    pkgs.python38Full
-  ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      # Needed for pygame
-      pkgs.glib
-      # Needed for matplotlib
-      pkgs.xorg.libX11
-    ];
-    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
-    LANG = "en_US.UTF-8";
-  };
+	deps = [
+		pkgs.clang_12
+		pkgs.ccls
+		pkgs.gdb
+		pkgs.gnumake
+	];
 }
